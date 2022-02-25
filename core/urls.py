98513dd6ef78462468f33409615c508be87a99ac
Rubel-Mahmud <rmsoft.com@gmail.com>
2022-02-25 10:33:35 +0600

    permissions implementated

--- core/urls.py
+++ core/urls.py
@@ -5,3 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('example.urls')),
 ]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
--- example/views.py
+++ example/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
 from api.serializers import IndustrySerializer, CompanySerializer, PersonSerializer
 from .models import Industry, Company, Person
 
@@ -6,11 +7,14 @@ from .models import Industry, Company, Person
 class IndustryViewSet(ModelViewSet):
     queryset = Industry.objects.all()
     serializer_class = IndustrySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class CompanyViewSet(ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class PersonViewSet(ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
